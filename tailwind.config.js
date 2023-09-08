6b337f74a14e28289c8388dd5ee92309cb2fcf5d
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-09-07 22:21:10 -0300

    feat: Adição de cores no Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      // Adição de cores no Tailwind:
+      colors: {
+        'red-lg': '#ff8d8d',
+        'yellow-lg': '#f7fc0e',
+      },
       // Configurando fontes no Tailwind:
       fontFamily: {
-      'jura': ['Jura', 'sans-serif']
+      'jura': ['Jura', 'sans-serif'],
+      'inter': ['Inter', 'sans-serif'],
     },
     },
   },
